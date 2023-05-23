af2f1ed0fcd4410fd6afe43829e07065e1bdd06b
feri-nurjaman04 <ferinaise@gmail.com>
2023-05-23 08:53:29 +0800

    test compression

---
+++ dist/js/script.js
@@ -0,0 +1,6 @@
+// let preload = document.getElementById('preload')
+
+// window.addEventListener('load', setTimeout(function(){
+//   preload.classList.remove('on')
+//   preload.classList.add('off')
+// }, 100))
